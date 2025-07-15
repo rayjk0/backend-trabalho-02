afc6a0b4549281c46aae9a7a3320ad2caf6f66d0
rayjk0 <raykaneressantos@gmail.com>
2025-07-15 09:40:39 -0300

    continuação

--- livros.ts
+++ livros.ts
@@ -12,31 +12,44 @@ interface Livro extends RowDataPacket {
   imagem: string;
 }
 export async function livrosRoutes(fastify: FastifyInstance) {
+  // GET 
   fastify.get('/livros', async () => {
-    const [rows] = await db.query('SELECT * FROM livros');
+    const [rows] = await db.query<Livro[]>('SELECT * FROM livros');
     return rows;
   });
 
-  ///get
+  // GET 
   fastify.get('/livros/:id', async (request) => {
     const { id } = request.params as { id: string };
-    const [rows] = await db.query('SELECT * FROM livros WHERE id = ?', [id]);
+    const [rows] = await db.query<Livro[]>('SELECT * FROM livros WHERE id = ?', [id]);
     return rows[0] || { erro: 'Livro não encontrado' };
   });
-///post 
+
+  // POST
   fastify.post('/livros', async (request, reply) => {
-    const { titulo, autor, ano, genero, preco, imagem } = request.body as any;
+    const { titulo, autor, ano, genero, preco, imagem } = request.body as {
+      titulo: string;
+      autor: string;
+      ano: number;
+      genero: string;
+      preco: number;
+      imagem: string;
+    };
+
     await db.query(
       'INSERT INTO livros (titulo, autor, ano, genero, preco, imagem) VALUES (?, ?, ?, ?, ?, ?)',
       [titulo, autor, ano, genero, preco, imagem]
     );
+
     reply.code(201).send({ mensagem: 'Livro adicionado com sucesso' });
   });
 
-  ////delet
+  // DELETE 
   fastify.delete('/livros/:id', async (request, reply) => {
     const { id } = request.params as { id: string };
+
     await db.query('DELETE FROM livros WHERE id = ?', [id]);
+
     reply.send({ mensagem: 'Livro deletado com sucesso' });
   });
 }
